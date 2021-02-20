a3f77c7ea47b6db8b32866f95047fb08849c6d7b
Crowdbotics <team@crowdbotics.com>
2021-02-20 23:11:09 +0000

    Update models

--- backend/crimson_hq_24639/wsgi.py
+++ backend/crimson_hq_24639/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crimson_hq_24639.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crimson_hq_24639.settings")
 
 application = get_wsgi_application()
